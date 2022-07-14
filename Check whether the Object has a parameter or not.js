3c839ca720bd3a32ea7eff40ba4b2673812c2923
Dhiraj sigh <106054448+dhiraj2611@users.noreply.github.com>
2022-07-14 16:19:32 +0530

    Add files via upload

---
+++ Check whether the Object has a parameter or not.js
@@ -0,0 +1,4 @@
+function Check(obj1) {
+    return(((Object.entries(obj1)).length === 0) ? 'false' : 'true' )
+  }
+  
---
+++ Merge the Objects.js
@@ -0,0 +1,5 @@
+function Check(obj1,obj2) {
+    let merge = Object.assign(obj1,obj2);
+     return merge;  
+ }
+ 
